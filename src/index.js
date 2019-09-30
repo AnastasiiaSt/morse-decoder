4bb7efd331cb514632855924c4d8871d4a0bb4cf
AnastasiiaSt <55129889+AnastasiiaSt@users.noreply.github.com>
2019-09-30 19:39:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr1 = Object.entries(MORSE_TABLE);
+    let arr2 = [];
+    let arr3 = [];
+    let result = "";
+
+	for (i = 0; i < arr1.length; i++) {
+
+    arr2[i] = "";
+    arr3[i]= arr1[i][1];
+
+    for (j = 0; j < arr1[i][0].length; j++) {
+        if (arr1[i][0].charAt(j) === ".") {
+            arr2[i] = arr2[i].concat("10");
+        } 
+        if (arr1[i][0].charAt(j) === "-") {
+            arr2[i] = arr2[i].concat("11");
+        }
+    }
+
+        if (arr2[i].length < 10) {
+           arr2[i] = arr2[i].padStart(10, "0");
+       }
+    }
+
+    arr2[arr2.length] = "**********";
+    arr3[arr3.length] = " ";
+
+	for (g = 0; g < expr.length; g += 10) {
+        result = result.concat(arr3[arr2.indexOf(expr.slice(g, g + 10))]);
+    }
+    return result;   
 }
 
 module.exports = {
